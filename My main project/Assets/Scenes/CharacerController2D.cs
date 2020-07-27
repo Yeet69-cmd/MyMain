45755e3d5f3bcc31c15fff707cce7cdbb024d042
Yeet 69 <selimkadhi@gmail.com>
2020-07-27 13:39:02 +0100

    1

---
+++ My main project/Assets/Scenes/CharacerController2D.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacerController2D : MonoBehaviour
+{
+    public float jumpForce;
+    public float speed;
+    public float smooth;
+    Rigidbody2D rb2d;
+    bool facingRight = true;
+    public LayerMask groundLayer;
+    Animator animator;
+    public bool isGrounded;
+    public Transform groundcheck;
+    public LayerMask whatisGround;
+    int chest;
+    private float x;
+    public float maxspeed;
+    public float normalspeed;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = true;
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+        maxspeed = speed * 2;
+        normalspeed = speed;
+
+
+    }
+
+    private void Update()
+    {
+        isGrounded = groundcheck.GetComponent<Collider2D>().IsTouchingLayers(whatisGround);
+
+
+        //get input 
+        x = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            isGrounded = false;
+            rb2d.velocity = new Vector2(0f, jumpForce);
+        }
+
+      
+
+        if (rb2d.velocity.y == 0)
+        {
+            animator.SetBool("Jumping", false);
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+            animator.SetBool("Jumping", true);
+        }
+
+        //if the input is moving the player right and the player is facing left...
+        if (x > 0 && !facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (x < 0 && facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+       
+        if (Input.GetKeyDown("s"))
+        {
+            StartCoroutine(GoFaster());
+        }
+      
+
+    }
+
+
+    IEnumerator GoFaster()
+    {
+        speed = maxspeed;
+        yield return new WaitForSeconds(5f);
+        speed = normalspeed;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (x != 0)
+        {
+            animator.SetBool("Running", true);
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+        }
+
+        //update the player's velocity
+        Vector2 Targetvelocity = new Vector2(x * speed, rb2d.velocity.y);
+
+        rb2d.velocity = Vector2.SmoothDamp(rb2d.velocity, Targetvelocity, ref Targetvelocity, Time.deltaTime * smooth);
+
+
+
+    }
+    void Flip()
+    {
+        //Invert the facingRight variable
+        facingRight = !facingRight;
+
+        //Flip the Character
+        Vector2 scale = transform.localScale;
+
+        scale.x *= -1;
+
+        transform.localScale = scale;
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "fire")
+        {   
+            //Destroy the star
+            Destroy(other.gameObject);
+            //Change the player's tag
+            gameObject.tag = "PowerUp";
+            //Change the player's color
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+            //Star the corountine
+            StartCoroutine(Reset());
+        }
+        if (other.CompareTag("chest"))
+        {
+            chest++;
+            Destroy(other.gameObject);
+        }
+        if (other.CompareTag("portal"))
+        {
+            if (chest > 5)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+            }
+        }
+
+
+    }
+    IEnumerator Reset()
+    {
+        //Declare the waiting time
+        yield return new WaitForSeconds(5F);
+        //Change the player's tag back
+        gameObject.tag = "Player";
+        //Change the player's color back
+        gameObject.GetComponent<Renderer>().material.color = Color.white;
+
+
+    }
+  
+
+}
---
+++ My main project/Assets/Scenes/ScoreManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    
+    int score;
+    public TextMeshProUGUI scoretxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (score > PlayerPrefs.GetInt("Highscore"))
+        {
+            PlayerPrefs.SetInt("Highscore", score);
+
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "chest")
+        {   //Adding 1 to the score
+            score++;
+            //Destroying the chest
+            Destroy(col.gameObject);
+            //Transforming our score value to a text
+            scoretxt.text = score.ToString();
+            PlayerPrefs.SetInt("savedscore", score);
+
+
+
+        }
+
+    }
+
+    
+}
