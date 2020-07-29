038ef5d8a314c09852da593fd3fb4c5339a54212
Yeet 69 <selimkadhi@gmail.com>
2020-07-29 11:19:41 +0100

    more fixing

--- My main project/Assets/scripts/Patrol.cs
+++ My main project/Assets/scripts/Patrol.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
-
+    public float originalSpeed;
     public float speed;
     public Transform startPoint, endPoint;
 
     private void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        originalSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
 
-        if(transform.position.x >= endPoint.position.x)
+        if (transform.position.x >= endPoint.position.x)
         {
             transform.rotation = Quaternion.Euler(Vector3.up * 180);
-            GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+            speed = -originalSpeed;
 
         }
         if (transform.position.x <= startPoint.position.x)
         {
-            transform.rotation = Quaternion.Euler(Vector3.zero);
-            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
-
+            transform.rotation = Quaternion.Euler(Vector3.zero); 
+            speed = originalSpeed;
 
         }
 
