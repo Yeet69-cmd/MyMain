6b01f23471178e3bf0205f695399cd28d4842efa
Yeet 69 <selimkadhi@gmail.com>
2020-07-29 14:43:03 +0100

    1

---
+++ My main project/Assets/enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        //Play hurt animation
+        animator.SetTrigger("Hurt");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+
+        }
+
+    }
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+
+        animator.SetBool("IsDead", true);
+
+        Destroy(gameObject, 1);
+
+    }
+
+
+
+}
--- My main project/Assets/scripts/HealthManager.cs
+++ My main project/Assets/scripts/HealthManager.cs
@@ -1,24 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
+
+    
+    public float MAXHEALTH = 10f;
+    float health;
+    public Gradient gradient;
+    public Image fill;
+    public Slider healthSlider;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = MAXHEALTH;
+
+        healthSlider.value = health / MAXHEALTH;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void TakeDamage(float amount)
     {
-        
+        health -= amount;
+
+        if (health <= 0)
+        {
+            health = 0;
+            Die();
+        }
+        // UPDATE THE SLIDER
+        healthSlider.value = health / MAXHEALTH;
     }
 
-    public void TakeDamage(float damage)
+    void Die()
     {
+        GetComponent<CharacerController2D>().enabled = false;
+        GetComponent<Animator>().SetBool("Dead", true);
+    }
+    public void GetHealth(float amount)
+    {
+        health += amount;
+
+        if (health > MAXHEALTH)
+        {
+            health = MAXHEALTH;
+        }
 
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Heal"))
+        {
+            GetHealth(5);
+            Destroy(other.gameObject);
+        }
+        // UPDATE THE SLIDER
+        healthSlider.value = health / MAXHEALTH;
 
+
+    }
 }
